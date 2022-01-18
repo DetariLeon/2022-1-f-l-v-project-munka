69aefc9704a2b3358d14b0ffb91002a9b7f18837
David95406 <83376469+David95406@users.noreply.github.com>
2022-01-18 21:34:44 +0100

    test2.4

--- !main.py
+++ !main.py
@@ -13,23 +13,38 @@ elegsegesdb = 0
 elegtelendb = 0
 db = 0
 tantargyak = []
-tantargy = ()
-tantargyszam = ()
+tantargy = 0
+tantargyszam = 0
 atlagok = []
-teljesatlag = ()
+teljesatlag = 0
+szoveg = ""
+tszoveg = []
+sor = "\n"
+sorok = []
+lefut = False
 
-print("Átlag kiszámító program\n Készítette: Ábrahám Dávid és Détári Leon")
-tantargyszam = int(input("Hány tantárgy átlagát szeretéd kiszámítani? "))
+print("Átlagszámító program\nKészítette: Ábrahám Dávid és Détári Leon")
+
+while lefut == False:
+    try:
+        tantargyszam = int(input("Hány tantárgy átlagát szeretnéd kiszámítani? "))
+        lefut = True
+    except:
+        print("nem jo :(") #LEON
 
 for t in range(tantargyszam):
-    print("Írja be az ", t + 1, ". tantárgy nevét!")
+    print("Írja be a(z) ", t + 1, ". tantárgy nevét!") 
     tantargy = input()
     tantargyak.append(tantargy)
 
 
+ido = asctime(localtime(time()))
+with open("atlag.txt", "a", encoding="utf8()") as file:
+    file.write(str(ido))
 
-for a in range(tantargyszam):
-    print("A(z) ", tantargyak[a], " átlaga")
+for i in range(tantargyszam):
+    sorok = []
+    print("A(z) ", tantargyak[i], " átlaga")
     while atlag != 0:
         if atlag == 0:
             break
@@ -50,22 +65,41 @@ for a in range(tantargyszam):
             elif atlag == 5:
                 jeles = jeles + 5
                 jelesdb = jelesdb + 1
+            else:
+                print("A szém legyen 1 és öt kozott") #leon
             db = db + 1 
     db = db - 1
     print()
-    print("A(z) ", tantargyak[a], " eredménye:")
+    sorok.append("\n")
+    print("A(z) ", tantargyak[i], " eredménye:")
+    sorok.append(f"A(z) {tantargyak[i]} eredménye:\n")
     print("----------------------------------------------")
+    sorok.append("----------------------------------------------\n")
     print("Jelesek száma: ", jelesdb, "db")
-    print("Jó-k száma: ", jodb, "db")
+    sorok.append(f"Jelesek száma: {jelesdb} db\n")
+    print("Jók száma: ", jodb, "db")
+    sorok.append(f"Jók száma: {jodb} db\n")
     print("Közepesek száma: ", kozepesdb, "db")
-    print("Elegseges db: ", elegsegesdb, "db")
-    print("Elegtelen db: ", elegtelen, "db")
-    print("----------------------------------------------")
+    sorok.append(f"Közepesek száma: {kozepesdb} db\n")
+    print("Elégsegesek száma: ", elegsegesdb, "db")
+    sorok.append(f"Elégsegesek száma: {elegsegesdb} db\n")
+    print("Elégtelenek száma: ", elegtelen, "db") 
+    sorok.append(f"Elégtelenek száma: {elegtelen}\n")
+    print("----------------------------------------------") #ez sor2
     print("Jegyeid száma: ", db)
+    sorok.append(f"Jegyeid száma: {db}\n")
     atlag = (jeles + jo + kozepes + elegseges + elegtelen) / db
     atlagok.append(atlag)
     print("Az átlagod: ", atlag, "\n")
-    
+    sorok.append(f"Az átlagod: {atlag} \n\n")
+
+    with open("atlag.txt", "a", encoding="utf8()") as file:
+        file.write("\n")
+        file.writelines(sorok)
+        file.write("")
+
+
+        
     #valtozo reset
     atlag = "a"
     jeles = 0
@@ -83,12 +117,14 @@ for a in range(tantargyszam):
 
 for b in range(len(atlagok)):
     teljesatlag = teljesatlag + atlagok[b]
-teljesatlag = teljesatlag / b
+teljesatlag = teljesatlag / len(atlagok)
 
-print(teljesatlag) 
+with open("atlag.txt", "a", encoding="utf8()") as file:
+    sor1 = f"Az összes tantárgyad teljes átlaga: {teljesatlag}"
+    szoveg = str(sor1)
+    file.write(szoveg)
+    file.write("----------------------------------------------")
 
-
-
-
-print("Az összes tantárgy teljes átlaga: ", teljesatlag)    
-input("A kilépéshez nyomjon [ENTER]")
+print("Az összes tantárgyad teljes átlaga: ", teljesatlag)  
+print("Az összes eredményt mentsük egy .txt-be")  
+input("A kilépéshez nyomjon egy [ENTER]-t")
